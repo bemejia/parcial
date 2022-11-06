b1b9a14f9b059097012aa2cfd4eb89e64790ee72
bemejia <b.mejial@uniandes.edu.co>
2022-11-05 21:59:25 -0500

    ajustes en la clase y servicio

--- src/app/planta/planta.service.spec.ts
+++ src/app/planta/planta.service.spec.ts
@@ -2,15 +2,40 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { PlantaService } from './planta.service';
+import {HttpClientModule} from "@angular/common/http";
+import {Planta} from "./planta";
 
-describe('Service: Planta', () => {
+describe('PlantaService', () => {
+  let service: PlantaService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [PlantaService]
+      providers: [HttpClientModule]
+    });
+    service = TestBed.inject(PlantaService);
+  });
+
+  it('should get the data successfully', (done: DoneFn) => {
+    service.getPlantas().subscribe({
+      next: (plantas) => {
+        expect(Array.isArray(plantas)).toBeTrue();
+        for (const planta of plantas){
+          expect(planta.id).toBeTruthy()
+          expect(planta.nombreComun).toBeTruthy()
+          expect(planta.tipo).toBeTruthy()
+          expect(planta.clima).toBeTruthy()
+          expect(planta.imagen).toBeTruthy()
+
+        }
+
+        done();
+      }
     });
   });
 
-  it('should ...', inject([PlantaService], (service: PlantaService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+
 });
--- src/app/planta/planta.ts
+++ src/app/planta/planta.ts
@@ -1,23 +1,43 @@
 export class Planta {
 
-  id: number;
-  nombreComun: string;
-  tipo: string;
-  clima: string;
-  imagen: string;
+  _id: number;
+  _nombreComun: string;
+  _tipo: string;
+  _clima: string;
+  _imagen: string;
 
-  constructor(
+  constructor(planta:{
     id: number,
     nombreComun: string,
     tipo: string,
     clima: string,
     imagen: string,
-  ){
-    this.id=id;
-    this.nombreComun=nombreComun;
-    this.tipo=tipo;
-    this.clima=clima;
-    this.imagen=imagen;
+  }){
+    this._id=planta.id;
+    this._nombreComun=planta.nombreComun;
+    this._tipo=planta.tipo;
+    this._clima=planta.clima;
+    this._imagen=planta.imagen;
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  get nombreComun(): string {
+    return this._nombreComun;
+  }
+
+  get tipo(): string {
+    return this._tipo;
+  }
+
+  get clima(): string {
+    return this._clima;
+  }
+
+  get imagen(): string {
+    return this._imagen;
   }
 
 }
